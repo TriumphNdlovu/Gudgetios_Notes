47e408ca6ea5658ac61fddb1740b3eeb531e9dc6
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2023-11-23 13:43:57 +0200

    fixed the responsiveness

--- gudgetios/app/Pages/Todolist/page.tsx
+++ gudgetios/app/Pages/Todolist/page.tsx
@@ -1,63 +1,72 @@
-
-'use client'
-import { CheckboxGroup, Checkbox, Button, Input, Divider,
-   Card, Table, TableBody, TableCell, TableColumn, 
-   TableHeader, TableRow, getKeyValue, Tabs,Tab, Select, SelectItem, Textarea } from "@nextui-org/react";
-import React, { useState, useEffect, } from "react";
-import { Todo } from '../../interfaces/TodoList';
-import { 
-        getTodosService,
-        addTodoService,
-        removeTodoService,
-        toggleTodoService 
-
-      } from '../../services/TodoService';
-import { 
-  FaEdit, FaTrash, FaPlus,FaCheck 
-
-} from 'react-icons/fa';
-
+'use client';
+import {
+  CheckboxGroup,
+  Checkbox,
+  Button,
+  Input,
+  Divider,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  getKeyValue,
+  Tabs,
+  Tab,
+  Select,
+  SelectItem,
+  Textarea,
+} from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
+import { Todo } from "../../interfaces/TodoList";
+import {
+  getTodosService,
+  addTodoService,
+  removeTodoService,
+  toggleTodoService,
+} from "../../services/TodoService";
+import { FaEdit, FaTrash, FaPlus, FaCheck } from "react-icons/fa";
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [newTodo, setNewTodo] = useState('');
-  const [dueDate, setDueDate] = useState('');
+  const [newTodo, setNewTodo] = useState("");
+  const [dueDate, setDueDate] = useState("");
   const [loading, setLoading] = useState(true);
-  const [filter, setFilter] = useState('Active');
-  const columns = ["Status", "Content","Due", "Action"];
+  const [filter, setFilter] = useState("Active");
+  const columns = ["Status", "Content", "Due", "Action"];
   const filtertitle = ["Active", "Completed", "Overdue"];
-
 
   useEffect(() => {
     getTodosService()
-      .then(initialTodos => {
-        const todosWithCorrectCompleted = initialTodos.map(todo => ({
+      .then((initialTodos) => {
+        const todosWithCorrectCompleted = initialTodos.map((todo) => ({
           ...todo,
           completed: Boolean(todo.completed),
         }));
         setTodos(todosWithCorrectCompleted);
-    
       })
       .finally(() => setLoading(false)); // Update loading state when done
   }, []);
 
   const handleAddTodo = () => {
-  
-    addTodoService(newTodo,dueDate).then(() => {
+    addTodoService(newTodo, dueDate).then(() => {
       getTodosService().then(setTodos);
-      setNewTodo('');
-      setDueDate('');
+      setNewTodo("");
+      setDueDate("");
     });
   };
 
-  const handleRemoveTodo = (uniqueId:string) => {
-
+  const handleRemoveTodo = (uniqueId: string) => {
     setLoading(true);
-    removeTodoService(uniqueId).then(() => getTodosService()).then(setTodos)
-    .finally(() => setLoading(false));
+    removeTodoService(uniqueId)
+      .then(() => getTodosService())
+      .then(setTodos)
+      .finally(() => setLoading(false));
 
     if (loading) {
-      return <p>Loading...</p>; 
+      return <p className="w-full justify-centre">Loading...</p>;
     }
   };
 
@@ -66,9 +75,9 @@ export default function TodoList() {
     const due = new Date(dueDate);
     return due < today;
   };
-  
-  const handleToggleTodo = (completed: boolean , uniqueId:string) => {
-    toggleTodoService(!completed , uniqueId).then(() => {
+
+  const handleToggleTodo = (completed: boolean, uniqueId: string) => {
+    toggleTodoService(!completed, uniqueId).then(() => {
       getTodosService().then(setTodos);
     });
   };
@@ -76,120 +85,130 @@ export default function TodoList() {
   if (loading) {
     return <p>Loading...</p>;
   }
-   
 
   return (
-    <div className="flex flex-col items-center justify-center px-4 sm:px-0">
+    <div className="flex flex-col items-center justify-center px-4 ">
       <div className="w-full max-w-full">
-        <Textarea
+
+      <div className="flex">
+      <Textarea
           label="Add Todo"
           variant="bordered"
           placeholder="enter todo here..."
           disableAnimation
           disableAutosize
+          value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
-          className="mb-2 w-full"
+          className="mb-2 w-full mr-2"
           classNames={{
             input: "resize-y min-h-[40px]",
           }}
         />
-        
         <div>
-
+          <p>Choose due Date</p>
           <Input
             type="date"
             value={dueDate}
             onChange={(e) => setDueDate(e.target.value)}
-            className="mb-2 w-full"
+            className="mb-2 w-full min-h-fit-content"
           />
-
-          <Divider orientation="vertical" />
-
-          <Button onClick={handleAddTodo} className="mb-2 w-full">
-            <FaCheck />
-          </Button>
-
         </div>
         
-  
-        <Select
-          items={filtertitle}
-          label="Filter"
-          placeholder="Filter Tasks"
-          className="mb-2 w-full"
-          onChange={(event) => setFilter(event.target.value)}
-        >
-          {filtertitle.map((item) => (
-            <SelectItem key={item} value={item}>
-              {item}
-            </SelectItem>
-          ))}
+      </div>
 
-        </Select>
+      <div className="flex">
+          <Button onClick={handleAddTodo} className="mb-2 flex-auto">
+            <FaPlus />
+          </Button>
+      </div>
+
+        
 
         <Divider className="mb-2" />
-
-        <Table className="mb-2 w-full">
+      <div className="w-full h-full">
+        <Table className="h-full w-full">
           <TableHeader>
-            <TableColumn key={0} className="w-1/6">{columns[0]}</TableColumn>
-            <TableColumn key={1} className="w-3/6">{columns[1]}</TableColumn>
-            <TableColumn key={2} className="w-1/6">{columns[2]}</TableColumn>
-            <TableColumn key={3} className="w-1/6">{columns[3]}</TableColumn>
+            {columns.map((column, index) => (
+              <TableColumn key={index} className={`w-${index === 1 ? 3 : 1}/6`}>
+                <div>
+                  {column}
+                  {index === 1 && 
+                      <Select
+                      items={filtertitle}
+                      label="Filter"
+                      key= "Active"
+                      placeholder="Filter Tasks"
+                      style={{ width: "40%", marginLeft: "30%"}}
+                      onChange={(event) => setFilter(event.target.value)}
+                      >
+                      {filtertitle.map((item) => (
+                        <SelectItem key={item} value={item}>
+                          {item}
+                        </SelectItem>
+                      ))}
+                    </Select>
+                  }
+                </div>
+                
+              </TableColumn>
+            ))}
           </TableHeader>
 
-                        <TableBody>
-                          {todos
-                          .filter(todo => {
-                            if (filter === 'Active') return !todo.completed;
-                            if (filter === 'Completed') return todo.completed;
-                            if (filter === 'Overdue') return (isOverdue(todo.due.toString()) && !todo.completed);
-                            return true;
-                          })
-                          .map((row) => (
-                            <TableRow key={row.uniqueId}
-                            style=
-                            {{ 
-                              backgroundColor: row.completed ? '#427b3f' : 'transparent',
-                            }}
-                            >
-                              <TableCell key={0}
-                                className="flex justify-center items-center rounded-lg"
-                              >
-                              {
-                                <input
-                                type="checkbox"
-                                checked={row.completed}
-                                onChange={() => handleToggleTodo(row.completed, row.uniqueId)} 
-                                className="w-4 h-4 sm:w-6 sm:h-6"
-                              />
-                              }
-                                
-                                </TableCell>
-                              <TableCell key={1}
-                                style={{
-                                  backgroundColor: row.completed ? '#transparent' : '#0D1317',
-                                  borderRadius: '10px',
-                                }}>
-                                {row.content}
-                              {row.content}</TableCell>
-                              <TableCell key={2}>{(row.due).toString()}</TableCell>
-                              <TableCell key={3}>
-                                {
-                                  <Button onClick={() => handleRemoveTodo(row.uniqueId)}>
-                                    <FaTrash/>
-                                  </Button>
-                                }
-                                {
-                                  <Button>
-                                    <FaEdit/>                              
-                                  </Button>
-                                }
-                              </TableCell>
-                            </TableRow>
-                          ))}
+          <TableBody>
+            {todos
+              .filter((todo) => {
+                if (filter === "Active") return !todo.completed;
+                if (filter === "Completed") return todo.completed;
+                if (filter === "Overdue")
+                  return isOverdue(todo.due.toString()) && !todo.completed;
+                return true;
+              })
+              .map((row) => (
+                <TableRow
+                  key={row.uniqueId}
+                  style={{
+                    backgroundColor: row.completed ? "#427b3f" : "transparent",
+                  }}
+                >
+                  <TableCell
+                    key={0}
+                    className="flex justify-center items-center rounded-lg"
+                  >
+                    <input
+                      type="checkbox"
+                      checked={row.completed}
+                      onChange={() => handleToggleTodo(row.completed, row.uniqueId)}
+                      className="w-4 h-4 sm:w-6 sm:h-6"
+                    />
+                  </TableCell>
+                  <TableCell
+                    key={1}
+                    style={{
+                      backgroundColor: row.completed
+                        ? "transparent"
+                        : "#0D1317",
+                      borderRadius: "10px",
+                    }}
+                  >
+                    {row.content}
+                  </TableCell>
+                  <TableCell key={2}>{row.due.toString()}</TableCell>
+                  <TableCell key={3}>
+                    <Button onClick={() => handleRemoveTodo(row.uniqueId)}>
+                      <FaTrash />
+                    </Button>
+
+                    <Button >
+                      <FaEdit />
+                    </Button>
+
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
-        </Table>           
+        </Table>
         </div>
       </div>
+    </div>
   );
 }
--- gudgetios/app/page.tsx
+++ gudgetios/app/page.tsx
@@ -1,31 +1,22 @@
-
 import MenuComponent from './components/MenuComponent';
 import TodoList from './Pages/Todolist/page';
 import Header from './components/Header';
 
-
 export default function Index() {
-
-
   return (
-  <div className="flex flex-col justify-between min-h-screen min-w-full w-full sm:w-1/4 md:w-1/3 lg:w-1/2 xl:w-2/3">
+    <div className="flex flex-col justify-between min-h-screen w-full ">
 
       <div className="sticky top-0 z-50">
         <MenuComponent />
       </div>
 
       <div>
-        <Header/>
+        <Header />
       </div>
 
-      <div className="flex items-center justify-center">
-        <TodoList/>
+      <div className=" w-full">
+        <TodoList />
       </div>
-
-      {/* <div className="flex items-center justify-center w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4">
-        <TodoList/>
-      </div> */}
-      
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
