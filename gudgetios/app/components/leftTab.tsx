e4e676ae47a43c08d68b2cb38c6f44da86da2a59
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2023-12-09 02:54:32 +0200

    Update leftTab component with new icons and styling

--- gudgetios/app/components/leftTab.tsx
+++ gudgetios/app/components/leftTab.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import AuthButton from './AuthButton';
-import { FaArrowRight } from 'react-icons/fa';
+import { FaArrowRight, FaGithubSquare, FaLinkedinIn, FaMailchimp } from 'react-icons/fa';
+import { RxCross1 } from "react-icons/rx";
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, button } from '@nextui-org/react';
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 export default function LeftTab() {
   const [isOpen, setIsOpen] = useState(true);
@@ -27,32 +30,64 @@ export default function LeftTab() {
   return (
     <>
       {isOpen && (
-        <div className="h-screen w-64 fixed left-0 top-0 bg-gray-800 text-white z-50">
-          <div className="p-6">
-            <h1 className="text-2xl font-bold mb-4">Menu</h1>
-            <ul>
-              <li className="mb-2">
-                <a href="#home" className="text-white">Home</a>
-              </li>
-              <li className="mb-2">
-                <a href="#about" className="text-white">About</a>
-              </li>
-              <li className="mb-2">
-                <a href="#contact" className="text-white">Contact</a>
-              </li>
-            </ul>
-            <button onClick={() => setIsOpen(false)}>Close</button>
+        <Card className="h-screen w-64 fixed left-0 top-0 text-white z-50">
+        <CardHeader className="text-2xl font-bold mb-4 relative">
+          <div className='flex justify-start'>
+            Menu
           </div>
+          <button onClick={() => setIsOpen(false)} 
+          className='absolute top-1 right-0 mt-2 mr-2 hover:-rotate-12'>
+            <RxCross1/>
+          </button>
+        </CardHeader>
+        <CardBody>
+          The body baba
+        </CardBody>
+
+
+
+
+        <CardFooter>
+        <div className='flex flex-col'>
+            <div className='flex justify-center text-centre'>
+              Contact us
+            </div>
+
+            <div className='py-2'>
+              <Divider />
+            </div>
+
+            <div >
+              
+                <Link href={'https://github.com/TriumphNdlovu'}>
+                  <FaGithubSquare/>
+                  Github
+                </Link>
+              
+              <button className='px-2 '>
+                <FaLinkedinIn className='flex justify-center text-centre'/>
+                Linkedin
+              </button >
+              <button className='px-2 '>
+                <FaMailchimp className='flex justify-center text-centre'/>
+                Gmail
+              </button>
+            </div>
+          </div>
+        </CardFooter>
+          
           {/* <AuthButton/> */}
-        </div>
+        </Card>
       )}
       {
         !isOpen && 
         <button 
           onClick={() => setIsOpen(true)} 
-          className="fixed left-0 top-1/2 transform -translate-y-1/2 z-50"
+          className="fixed left-0 top-1/2 transform -translate-y-1/2 z-50 text-2xl
+          hover:text-[50px]
+          "
         >
-          <FaArrowRight/>
+          <MdOutlineArrowForwardIos/>
         </button>
       }
     </>
