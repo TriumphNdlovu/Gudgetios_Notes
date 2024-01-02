ffbaf527f0ffddc112992da090ea45be8a2a5420
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2024-01-02 22:47:06 +0200

    Update task completion icon

--- gudgetios/app/Pages/Todolist/page.tsx
+++ gudgetios/app/Pages/Todolist/page.tsx
@@ -3,13 +3,13 @@ import { Button, Card, CardHeader } from "@nextui-org/react";
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import React, { useState } from 'react';
-import { FaCheck, FaChess } from "react-icons/fa";
+import { FaCheck, FaTrash } from "react-icons/fa";
 
 const ItemTypes = {
   CARD: 'card',
 };
 
-const CardItem = ({ id, content, index, moveCard }: { id: string, content: string, index: number, moveCard: (fromIndex: number, toIndex: number) => void }) => {
+const CardItem = ({ id, content, completed, index, moveCard }: { id: string, content: string, completed: boolean, index: number, moveCard: (fromIndex: number, toIndex: number) => void }) => {
   const [, ref] = useDrag({
     type: ItemTypes.CARD,
     item: { id, index },
@@ -17,7 +17,7 @@ const CardItem = ({ id, content, index, moveCard }: { id: string, content: strin
 
   const [, drop] = useDrop({
     accept: ItemTypes.CARD,
-    hover: (draggedItem:any) => {
+    hover: (draggedItem: any) => {
       if (draggedItem.index !== index) {
         moveCard(draggedItem.index, index);
         draggedItem.index = index;
@@ -28,16 +28,34 @@ const CardItem = ({ id, content, index, moveCard }: { id: string, content: strin
   return (
     <div ref={(node) => ref(drop(node))}>
       <div className="flex flex-row pt-1">
-          <Card className="bg-blue-400 h-10 w-full items-center">
-            <div >
-              {content}
-            </div>
-          </Card>
-          <div >
-            <Button >
+          <Card style={
+            completed ?
+            {
+               textDecoration: 'line-through',
+               backgroundColor: 'green'
+            }:
+            {
+              
+            }
+            
+          }
+          className=" bg-transparent border border-cyan-600 h-10 w-full justify-start pl-5">
+            {content}
+          </Card>  
+        <div>
+          <Button>
+          {
+            completed ?
+            (
+              <FaTrash/>
+            ) :
+            (
               <FaCheck/>
-            </Button>
-          </div>
+            )
+          }
+            
+          </Button>
+        </div>
       </div>
     </div>
   );
@@ -45,12 +63,15 @@ const CardItem = ({ id, content, index, moveCard }: { id: string, content: strin
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([
-    { id: 'task-1', content: 'Task 1' },
-    { id: 'task-2', content: 'Task 2' },
-    { id: 'task-3', content: 'Task 3' },
+    { id: 'task-1', content: 'Task 1', Completed: false },
+    { id: 'task-2', content: 'Task 2', Completed: false },
+    { id: 'task-3', content: 'Task 3', Completed: true },
+    { id: 'task-4', content: 'Trinity', Completed: false },
+    { id: 'task-5', content: 'Trevor', Completed: true },
   ]);
 
-  const moveCard = (fromIndex:any, toIndex:any) => {
+
+  const moveCard = (fromIndex: any, toIndex: any) => {
     const updatedTasks = [...tasks];
     const [movedTask] = updatedTasks.splice(fromIndex, 1);
     updatedTasks.splice(toIndex, 0, movedTask);
@@ -59,74 +80,37 @@ export default function TodoList() {
 
   return (
     <div className=" flex flex-col items-center">
-        <h1 className='text-2xl font-bold'>TodoList</h1>
-
-        <div>
-          <p>Add Button here</p>
-        </div>
-
-        <div>
-          <div className="flex flex-row items-center h-[80vh]">
-            <div className="px-1">
-              <Card className="flex-grow min-w-[60vw] min-h-[60vh]">
-                <CardHeader>
-                  Tasks
-                </CardHeader>
-                <div>
-                  <DndProvider backend={HTML5Backend}>
-                    {tasks.map((task, index) => (
-                      <CardItem key={task.id} 
-                      id={task.id} content={task.content} 
-                      index={index} moveCard={moveCard} 
+      <h1 className='text-2xl font-bold'>TodoList</h1>
+      <div>
+        <Button>Add Task</Button>
+      </div>
+      <div>
+        <div className="flex flex-row items-center h-[80vh]">
+          <div className="px-1">
+            <Card className="flex-grow min-w-[60vw] min-h-[60vh]">
+              <CardHeader>
+                Tasks
+              </CardHeader>
+              <div>
+                <DndProvider backend={HTML5Backend}>
+                  {tasks.map((task, index) => (
+                    
+                      <CardItem
+                        key={task.id}
+                        id={task.id}
+                        content={task.content}
+                        completed={task.Completed}
+                        index={index}
+                        moveCard={moveCard}
                       />
-                      ))}
-                      
-                  </DndProvider>
-                </div>
-
-              </Card>
-            </div>
+                  
+                  ))}
+                </DndProvider>
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
-
-      
     </div>
   );
 }
-
-
-
-
-{/* <div className=" flex flex-col items-center">
-        <h1 className='text-2xl font-bold'>TodoList</h1>
-
-        <div>
-          <p>Add Button here</p>
-        </div>
-        <div>
-          <div className="flex flex-row items-center h-[80vh]">
-
-            <div className="px-1">
-              <Card className="items-center flex-grow min-w-[20vw] min-h-[60vh]">
-                <p>Backlog</p>
-              </Card>
-            </div>
-
-              
-
-            <div className="px-1">
-              <Card className="items-center flex-grow min-w-[20vw] min-h-[60vh]">
-                <p>Inprogress</p>
-              </Card>
-            </div>
-
-              
-            <div className="px-1">
-              <Card className="items-center flex-grow min-w-[20vw] min-h-[60vh]">
-                <p className="items-center">Completed</p>
-              </Card>
-            </div>
-
-          </div>
-        </div>
-      </div> */}
